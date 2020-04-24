8790b22b261acb5bb5c06c42b620fa4668786b35
dveamer <dveamer@gmail.com>
2020-04-24 21:29:11 +0900

    Fallback 에서 Throwable 정보 로깅 추가

--- gateway/src/main/java/com/dveamer/gateway/FallbackController.java
+++ gateway/src/main/java/com/dveamer/gateway/FallbackController.java
@@ -1,16 +1,23 @@
 package com.dveamer.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class FallbackController {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/fallback")
-    public Mono<String> fallback() {
+    public Mono<String> fallback(ServerWebExchange exchange) {
+        Throwable exception = exchange.getAttribute(ServerWebExchangeUtils.CIRCUITBREAKER_EXECUTION_EXCEPTION_ATTR);
+        logger.debug("", exception);
         return Mono.just("fallback-gateway");
     }
-
 
 }
